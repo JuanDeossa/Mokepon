d69823f21ca3eb2a66224619de71c7320cd728e3
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-06 12:24:56 -0500

    loadpage event listener

---
+++ main.js
@@ -0,0 +1,20 @@
+// Golbar Var ----|
+
+// Golbar Var ---->
+
+
+// Document Element Assignments ----|
+
+// Document Element Assignments ---->
+
+
+// Event listeners ----|
+// Initial Loadpage
+window.addEventListener("load",initialCoinditions)
+// Event listeners ---->
+
+
+// Function Definitions ----|
+
+// Function Definitions ---->
+
