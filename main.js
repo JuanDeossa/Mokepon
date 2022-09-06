764938e14d652f265e0a4322727ecaf2bc0a3bb9
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-06 13:52:31 -0500

    Select Attack function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Golbar Var ----|
-
+let mokeponSelected;
 // Golbar Var ---->
 
 
@@ -26,12 +26,37 @@ function initialCoinditions() {
     attackMessage.classList.add("inactive");
     restart.classList.add("inactive");
 }
+//Select Mokepon (2).
+function selectMokepon() {
+    if (Hipodoge.checked) {
+        mokeponSelected = Hipodoge.id
+        selectAttack()
+    }else if (Capipepo.checked) {
+        mokeponSelected = Capipepo.id
+        selectAttack()
+    }else if (Ratigueya.checked) {
+        mokeponSelected = Ratigueya.id
+        selectAttack()
+    }else {
+        alert("Choose a Mokepon");
+    }
+    console.log(mokeponSelected);
+}
+// Select Attack (3).
+function selectAttack() {
+    mokeponSelection.classList.add("inactive")
+    stats.classList.remove("inactive");
+    attackSelection.classList.remove("inactive");
+}
 // Function Definitions ---->
 
 
 // Event listeners ----|
 // Initial Loadpage
 window.addEventListener("load",initialCoinditions)
+// 
+// Mokepon Selection Button 
+mokeponSubmitBtn.addEventListener("click",selectMokepon)
 // 
 // Event listeners ---->
 
