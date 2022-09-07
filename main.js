f64f894047d9d819247de53af69199e9f066a642
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-06 21:26:09 -0500

    Js worwing succesfully.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Golbar Var ----|
 let mokeponSelected;
-let playerVitalPoints = 2;
-let botVitalPoints = 2;
-let Attacks = ["Fire","Water","Earth"];
-let botAttackSelected;
+let playerVitalPoints = 2
+let botVitalPoints = 2
+let Attacks = ["Fire","Water","Earth"]
+let Mokepones = ["Hipodoge","Capipepo","Ratigueya"]
+let botAttackSelected
 const resultMessages = {
     won: "You Won",
     lose: "You Lose",
@@ -13,16 +14,16 @@ const resultMessages = {
 
 
 // Document Element Assignments ----|
-//Sections.
+//Sections
 const mokeponSelection = document.querySelector(".Mokepon-selection")
 const stats = document.querySelector(".Stats")
 const attackSelection = document.querySelector(".Attack-selection")
 const attackMessage = document.querySelector(".Attack-message")
 const restart = document.querySelector(".Restart")
-//Stats.
+//Stats
 const playerStats = document.querySelector(".Stats__player")
 const botStats = document.querySelector(".Stats__bot")
-//Mokepones.
+//Mokepones
 const Hipodoge = document.querySelector("#Hipodoge")
 const Capipepo = document.querySelector("#Capipepo")
 const Ratigueya = document.querySelector("#Ratigueya")
@@ -34,22 +35,23 @@ const earthAttack = document.querySelector(".Attack-selection_Earth")
 const playerAttack = document.querySelector(".Attack-message__player")
 const botAttack = document.querySelector(".Attack-message__bot")
 //Message
+const playerName = document.querySelector(".Player-Name")
+const botName = document.querySelector(".Bot-Name")
 const resultMessage = document.querySelector(".Attack-message__Result")
 // Document Element Assignments ---->
 
 
 // Function Definitions ----|
-//Initial Conditions (1).
+// Initial Conditions (1).
 function initialCoinditions() {
+    mokeponSelection.classList.remove("inactive")
     stats.classList.add("inactive");
     attackSelection.classList.add("inactive");
     attackMessage.classList.add("inactive");
     restart.classList.add("inactive");
-    playerStats.innerHTML=playerVitalPoints
-    botStats.innerHTML=botVitalPoints   
     // ---> OUT
 }
-//Select Mokepon (2).
+// Select Mokepon (2).
 function selectMokepon() {
     if (Hipodoge.checked) {
         mokeponSelected = Hipodoge.id
@@ -66,34 +68,44 @@ function selectMokepon() {
 }
 // Select Attack (3).
 function selectAttack() {
+    playerStats.innerHTML=playerVitalPoints
+    botStats.innerHTML=botVitalPoints   
+    playerName.innerHTML=mokeponSelected;
+    botName.innerHTML=randomItem(Mokepones);
     mokeponSelection.classList.add("inactive")// ---> Reset
     stats.classList.remove("inactive");// ---> Reset
     attackSelection.classList.remove("inactive");// ---> Reset
 }
 // Match Result (4).
 function matchResult() {        
-        let player = this.name
-        let bot = randomItem(Attacks);
-        if ((player==="Water" && bot==="Fire")||(player==="Earth" && bot==="Water")||(player==="Fire" && bot==="Earth")){
-            botVitalPoints--
-            resultMessage.innerHTML=resultMessages.won
-        }
-        else if(player===bot){
-            resultMessage.innerHTML=resultMessages.draw
-        }
-        else{
-            playerVitalPoints--
-            resultMessage.innerHTML=resultMessages.lose
-        }
-        if ((playerVitalPoints===0)||(botVitalPoints===0)) {            
-            attackSelection.classList.add("inactive")
-            restart.classList.remove("inactive")
-        }
-        attackMessage.classList.remove("inactive");
-        playerAttack.innerHTML=player
-        botAttack.innerHTML=bot
-        playerStats.innerHTML=playerVitalPoints
-        botStats.innerHTML=botVitalPoints
+    let player = this.name
+    let bot = randomItem(Attacks);
+    if ((player==="Water" && bot==="Fire")||(player==="Earth" && bot==="Water")||(player==="Fire" && bot==="Earth")){
+        botVitalPoints--
+        resultMessage.innerHTML=resultMessages.won
+    }
+    else if(player===bot){
+        resultMessage.innerHTML=resultMessages.draw
+    }
+    else{
+        playerVitalPoints--
+        resultMessage.innerHTML=resultMessages.lose
+    }
+    if ((playerVitalPoints===0)||(botVitalPoints===0)) {     
+        attackSelection.classList.add("inactive")
+        restart.classList.remove("inactive")
+    }
+    attackMessage.classList.remove("inactive");
+    playerAttack.innerHTML=player
+    botAttack.innerHTML=bot
+    playerStats.innerHTML=playerVitalPoints
+    botStats.innerHTML=botVitalPoints
+}
+// Restart Waiting (5).
+function restartGame() {
+    playerVitalPoints = 2;
+    botVitalPoints = 2;
+    initialCoinditions()
 }
 // Random Item
 function randomItem(Array) {
@@ -108,12 +120,15 @@ window.addEventListener("load",initialCoinditions)
 // 
 // Mokepon Selection Button 
 mokeponSubmitBtn.addEventListener("click",selectMokepon)
-// 
+//
 // Attack Buttons 
 fireAttack.addEventListener("click",matchResult)
 waterAttack.addEventListener("click",matchResult)
 earthAttack.addEventListener("click",matchResult)
-// 
+//
+// Restart Game
+restart.addEventListener("click",restartGame) 
+//
 // Event listeners ---->
 
 
